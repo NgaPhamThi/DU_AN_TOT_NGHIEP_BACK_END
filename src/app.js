98e2806f059276f779409ed6645e830faf18865e
tungph20842 <dasuabest@gmail.com>
2023-10-16 04:49:23 +0700

    category

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
-import express from "express"
+import express from "express";
 import mongoose from "mongoose";
-import commentRouter from "./routers/comment"
+import cors from "cors";
+import commentRouter from "./routers/comment";
+import categoryRouter from "./routers/category";
+
 const app = express();
 app.use(express.json())
 app.use(cors())
-app.use('/api',commentRouter)
+app.use('/api',commentRouter);
+app.use("/api", categoryRouter);
 mongoose.connect('mongodb://127.0.0.1:27017/DATN_WD55')
 export const viteNodeApp = app;

7710bfde5792e615dccfc152d939cb9f44bdd3d2
NgaPhamThi <nga909557@gmail.com>
2023-10-17 20:02:15 +0700

    update

--- src/app.js
+++ src/app.js
@@ -8,8 +8,8 @@ import colorRouter from "./routers/color"
 import sizeRouter from "./routers/size"
 import cartRouter from "./routers/cart"
 
-const app = express();
 
+const app = express();
 app.use(express.json());
 app.use(cors())
 app.use('/api', commentRouter);
@@ -19,4 +19,7 @@ app.use("/api", colorRouter)
 app.use("/api", sizeRouter)
 app.use("/api", cartRouter)
 mongoose.connect('mongodb://127.0.0.1:27017/DU_AN')
-export const viteNodeApp = app;
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/DATN_WD55')
+export const viteNodeApp = app

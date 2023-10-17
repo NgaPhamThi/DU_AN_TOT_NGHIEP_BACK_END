7896ad86a8fabd612a76484044c4a2838e12edfc
NgaPhamThi <nga909557@gmail.com>
2023-10-17 19:58:40 +0700

    update

--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
-import express from "express"
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import commentRouter from "./routers/comment";
+import categoryRouter from "./routers/category";
+import productRouter from "./routers/product";
 import colorRouter from "./routers/color"
 import sizeRouter from "./routers/size"
-import  cartRouter from "./routers/cart"
-import mongoose from "mongoose";
+import cartRouter from "./routers/cart"
+
 const app = express();
 
 app.use(express.json());
-
+app.use(cors())
+app.use('/api', commentRouter);
+app.use("/api", categoryRouter);
+app.use("/api", productRouter);
 app.use("/api", colorRouter)
 app.use("/api", sizeRouter)
 app.use("/api", cartRouter)

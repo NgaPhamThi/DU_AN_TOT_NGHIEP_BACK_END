81f106ee8cc0a91025257847261183005a73dd8f
NgaPhamThi <nga909557@gmail.com>
2023-10-17 14:15:44 +0700

    cart_color_size

--- src/app.js
+++ src/app.js
@@ -1,5 +1,14 @@
 import express from "express"
-
+import colorRouter from "./routers/color"
+import sizeRouter from "./routers/size"
+import  cartRouter from "./routers/cart"
+import mongoose from "mongoose";
 const app = express();
 
+app.use(express.json());
+
+app.use("/api", colorRouter)
+app.use("/api", sizeRouter)
+app.use("/api", cartRouter)
+mongoose.connect('mongodb://127.0.0.1:27017/DU_AN')
 export const viteNodeApp = app;

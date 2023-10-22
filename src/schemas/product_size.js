404d0155f5c124c5863d451449e4c4ecaeb395bd
quangpn <quangpn1254@gmail.com>
2023-10-22 22:13:03 +0700

    product_size

--- src/schemas/product_size.js
+++ src/schemas/product_size.js
@@ -2,7 +2,7 @@ import joi from "joi"
 
 export const productSizeSchema = joi.object({
     quantity: joi.number().required(),
-    // productId: joi.number().required(),
-    // colorId: joi.number().required(),
-    // sizeId: joi.number().required(),
+    productId: joi.string().required(),
+    colorId: joi.string().required(),
+    sizeId: joi.string().required(),
 })

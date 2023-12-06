79197b342c9e2ef586d386f0bad75cd440892c0a
NTienDuy <105479766+NTienDuy@users.noreply.github.com>
2023-12-06 17:40:39 +0700

    BE_News

--- src/app.js
+++ src/app.js
@@ -8,11 +8,12 @@ import voucherRouter from "./routers/voucher";
 import colorRouter from "./routers/color";
 import sizeRouter from "./routers/size";
 import cartRouter from "./routers/cart";
+import newsRouter from "./routers/news";
 import oderDetailRouter from "./routers/Oder_detail";
 import productSizeRouter from "./routers/product_size";
 import router from "./routers/users";
-import orderRouter from './routers/orders'
-import searchRouter from './routers/search'
+import orderRouter from "./routers/orders";
+import searchRouter from "./routers/search";
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -23,11 +24,14 @@ app.use("/api", voucherRouter);
 app.use("/api", colorRouter);
 app.use("/api", sizeRouter);
 app.use("/api", cartRouter);
+app.use("/api", newsRouter);
 app.use("/api", oderDetailRouter);
 app.use("/api", productSizeRouter);
-app.use("/api", router)
-app.use('/api',orderRouter)
-app.use('/api',searchRouter)
+app.use("/api", router);
+app.use("/api", orderRouter);
+app.use("/api", searchRouter);
 // mongoose.connect("mongodb://127.0.0.1:27017/DATN_WD55");
-mongoose.connect("mongodb+srv://phamvanduy15012003:vanduy2003@duantotnghiep.lcazero.mongodb.net/DuAn?retryWrites=true&w=majority");
+mongoose.connect(
+  "mongodb+srv://phamvanduy15012003:vanduy2003@duantotnghiep.lcazero.mongodb.net/DuAn?retryWrites=true&w=majority"
+);
 export const viteNodeApp = app;
